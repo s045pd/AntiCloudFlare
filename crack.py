b0bfa89a3c8be15b4269b64cd3221117c9001d52
ai3 <aoii103@126.com>
2019-11-21 21:08:42 +0800

    fixed problem

--- crack.py
+++ crack.py
@@ -1,6 +1,6 @@
 import json
 import time
-from urllib.parse import urljoin, urlparse, quote
+from urllib.parse import urljoin, urlparse, quote,unquote,urlencode
 
 import execjs
 import requests
@@ -9,6 +9,10 @@ from log import success, info, error, warning
 import click
 
 from contextlib import contextmanager
+
+
+import requests
+from hyper.contrib import HTTP20Adapter
 
 
 @contextmanager
@@ -82,6 +86,7 @@ def Crack(content, url, end_time, save_res=False):
             pass
 
     params = {i.attr("name"): i.attr("value") for i in jq_data("input").items()}
+    params['s'] = quote(params['s'])
     params["jschl_answer"] = execjs.eval(
         js_code[-3].replace(key_3, str(start_data[key_2]))
     )
@@ -102,28 +107,31 @@ def Crack(content, url, end_time, save_res=False):
 @click.option("--url", default="https://coinone.co.kr/", help="Target url.")
 def test(url):
     while True:
+        session = requests.Session()
+        session.mount(url.split("/")[0], HTTP20Adapter())
+        session.headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-GB,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+            "cache-control": "no-cache",
+            "dnt": "1",
+            "pragma": "no-cache",
+            "referer": url,
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+        }
+        resp = session.get(url)
         with checkTimes():
             end_time = time.time() + 4
-            session = requests.Session()
-            session.headers = {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-                "cache-control": "no-cache",
-                "dnt": "2",
-                "pragma": "no-cache",
-                "referer": url,
-                "upgrade-insecure-requests": "1",
-                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
-            }
-            resp = session.get(url)
             info(session.cookies)
-            info(
-                session.get(
-                    Crack(resp.text, resp.url, end_time, save_res=True),
-                    allow_redirects=False,
-                ).headers
+            resp = session.get(
+                Crack(resp.text, resp.url, end_time, save_res=True),
+                allow_redirects=True,
             )
+            info(resp.request.headers)
             error(f"cf_clearance: {session.cookies.get('cf_clearance','')}")
 
 
@@ -134,7 +142,7 @@ def test_local():
         url = "https://coinone.co.kr/"
         info(
             Crack(content, url, time.time(), False)
-            == "https://coinone.co.kr/cdn-cgi/l/chk_jschl?s=2923ae212ba0bc78667c36998d9318cddd4c1c54-1554883527-1800-AYwHB3rPS1DnyuTKbZnposynqvvMSVF3O%2F%2FlYTvABrXWrsAmkLBqwvafg1fJVVSnvpFZcbCmfL7sj3PJUQ%2BKXrCGNth0%2BcwMVY5s0w6aWyigjCF%2FSC7Infj3y46UopAW6A%3D%3D&jschl_vc=b934979895320cc3505fbb1ce949050c&pass=1554883531.55-JMlpBBaLZJ&jschl_answer=10.8761383509"
+            == "https://coinone.co.kr/cdn-cgi/l/chk_jschl?s=3c3bff10311f4a2a7465190684507b4b55e40886-1574326333-0-AcEt1Ao9a1Micfg%2FWTHqvRj025Iy6P9zXMcTdFbQ8w1rS3KmQtdqSCfIg9vFXrKQL%2FQl9c0AKKOizKUfcSr529MxwQB2EzgdCkTUcJmj55%2BDgFUG0H7sPtpz5Lq4lOUSl5peRNVWyQxmeSMgb5kUf%2BE%3D&jschl_vc=3ac74535829c1a46193f6272a1e9e29f&pass=1574326337.264-4c2z7JxfaY&jschl_answer=34.9853718953"
         )
 
 
